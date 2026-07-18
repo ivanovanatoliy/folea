f984eac8e224c3a1ce460673d431c7179ef6d44e
Anatoliy Ivanov <ivanov.an.se@gmail.com>
2026-07-19 00:17:42 +0500

    Fix Homebrew Spotlight alias creation

--- packaging/homebrew/Formula/folea.rb
+++ packaging/homebrew/Formula/folea.rb
@@ -1,3 +1,5 @@
+require "etc"
+
 class Folea < Formula
   desc "Development build of the keyboard-driven Typst note reader"
   homepage "https://github.com/ivanovanatoliy/folea"
@@ -32,18 +34,18 @@ class Folea < Formula
     rm prefix/"folea.app.zip"
     system "codesign", "--force", "--deep", "--sign", "-", prefix/"folea.app"
 
-    applications = Pathname(Dir.home)/"Applications"
+    applications = Pathname(Etc.getpwuid.dir)/"Applications"
     applications.mkpath
     app_alias = applications/"Folea Dev.app"
     rm_f app_alias
     system "osascript", "-l", "JavaScript", "-e", <<~JAVASCRIPT
       ObjC.import("Foundation");
-      const target = $.NSURL.fileURLWithPath(#{(opt_prefix/"folea.app").to_s.dump});
+      const target = $.NSURL.fileURLWithPath(#{(prefix/"folea.app").to_s.dump});
       const alias = $.NSURL.fileURLWithPath(#{app_alias.to_s.dump});
       const data = target.bookmarkDataWithOptionsIncludingResourceValuesForKeysRelativeToURLError(
-        $.NSURLBookmarkCreationSuitableForBookmarkFile, null, null, null
+        $.NSURLBookmarkCreationSuitableForBookmarkFile, $(), $(), $()
       );
-      if (!data || !$.NSURL.writeBookmarkDataToURLOptionsError(data, alias, 0, null)) {
+      if (!data || !$.NSURL.writeBookmarkDataToURLOptionsError(data, alias, 0, $())) {
         throw new Error("Could not create Spotlight alias");
       }
     JAVASCRIPT
